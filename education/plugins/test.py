5af08f1ca7da09f819ab862411fd5ff96f5b4317
LeeDo <79679194+RaOOns@users.noreply.github.com>
2025-11-06 15:13:31 +0900

    Update test.py

--- education/plugins/test.py
+++ education/plugins/test.py
@@ -5,30 +5,28 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-car_data_1_joined = dataiku.Dataset("car_data_1_joined")
-car_data_1_joined_df = car_data_1_joined.get_dataframe()
+input_data = dataiku.Dataset("input_dataset")
+input_data_df = input_data.get_dataframe()
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 columns_type = "string"
-row_int = 0
+row_cnt = 0
 
 if columns_type == "string":
-    py_code2_df = car_data_1_joined_df.select_dtypes(include=['object', 'string'])
+    output_data = input_data_df.select_dtypes(include=['object', 'string'])
 
 elif columns_type != "string":
-    py_code2_df = car_data_1_joined_df.select_dtypes(exclude=['object', 'string'])
+    output_data = input_data_df.select_dtypes(exclude=['object', 'string'])
     
 else:
-    py_code2_df = py_code2_df.copy()
-    
-    
+    output_data = output_data.copy()
     
 if row_cnt != 0:
-    py_code2_df = py_code2_df.iloc[:row_cnt, :]
+    output_data = output_data.iloc[:row_cnt, :]
 else:
     pass
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
-py_code2 = dataiku.Dataset("py_code2")
-py_code2.write_with_schema(py_code2_df)
+output_data_df = dataiku.Dataset("output_dataset")
+output_data_df.write_with_schema(output_data)
